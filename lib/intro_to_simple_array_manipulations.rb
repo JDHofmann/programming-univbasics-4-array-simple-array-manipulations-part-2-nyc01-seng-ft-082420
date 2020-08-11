0da521015745b7fc551981993a69ba08803ae8cd
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-11 22:07:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,10 @@ def using_flatten(array)
 end 
 
 def using_delete(array, string)
-  array.delete("string")
+  array.delete(string)
+  array 
+end 
+def using_delete_at (array, integer)
+  array.delete_at(integer)
   array 
 end 
